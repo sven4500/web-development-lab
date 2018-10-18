2b6d54df385684be27d0dcbb4c78b1df3ff6d8ec
Иварс Рожлейс <noname@noname.com>
2018-10-18 23:52:58 +0400

    Добавил метод для автоматического определения доступности ресурса.

--- scripts/part-1.js
+++ scripts/part-1.js
@@ -1,52 +1,59 @@
+function checkImage(fn) {
+	// Метод проверяет доступность ресурса.
+	// Инициируем HEAD-запрос чтобы проверить доступность ресурса
+	// без фактической загрузки его тела. Третий параметр установлен
+	// как false что означает что запрос инициируется асинхронно.
+	// Это важно потому что нужно дождаться ответа.
+	var h = new XMLHttpRequest();
+	h.open("HEAD", fn, false);
+	h.send();
+	return h.status != 404;
+}
+
 function constructHtmlTable(dt, rows) {
-	console.log(dt);
-	console.log(rows);
-	
 	var tb = "";
 	tb += "<table border=\"1\">";
 	
 	for(var i = 0; i < dt.data.length; ++i) {
 		tb += "<tr>";
 		
+		// исполнитель
 		if(rows[0] != 0) {
-			tb += "<th>";
-			tb += dt.data[i][0];
-			tb += "</th>";
+			tb += "<th>" + dt.data[i][0] + "</th>";
 		}
 		
-		if(rows[1] != 0) {
-			tb += "<th>";
-			tb += dt.data[i][1];
-			tb += "</th>";
-		}
-		
+		// альбом
 		if(rows[2] != 0) {
-			tb += "<th>";
-			tb += dt.data[i][2];
-			tb += "</th>";
+			tb += "<th>" + dt.data[i][2] + "</th>";
 		}
 		
+		// жанр
+		if(rows[1] != 0) {
+			tb += "<th>" + dt.data[i][1] + "</th>";
+		}
+		
+		// год
 		if(rows[3] != 0) {
-			tb += "<th>";
-			tb += dt.data[i][3];
-			tb += "</th>";
+			tb += "<th>" + dt.data[i][3] + "</th>";
 		}
 		
+		// обложка
 		if(rows[4] != 0) {
-			tb += "<th>";
-			if(dt.data[i][4] != "null" && dt.data[i][4] != "") {
-				tb += "<img src=\"covers/";
-				tb += dt.data[i][4];
-				tb += "\" />";
+			var fn = "covers/" + dt.data[i][4];
+			
+			// Проверяем доступность ресурса. Если недоступен,
+			// то заменяем обложку на рисунок по-умолчанию.
+			if(checkImage(fn) == false) {
+				fn = "covers/default.jpg";
 			}
-			tb += "</th>";
+			
+			tb += "<th>" + "<img src=\"" + fn + "\" />" + "</th>";
 		}
 		
 		tb += "</tr>";
 	}
 	
 	tb += "</table>";
-	console.log(tb);
 	return tb;
 }
 
@@ -57,14 +64,14 @@ function g(data, stat) {
 	var dt = Papa.parse(data, {encoding: "utf-8"});
 	
 	// Преобразовываем массив объектов в массив переключателей.
+	// Если в массиве присутствует объект это значет что переключатель
+	// включен. Поэтому все присутствующие оюекты это включеные переключатели.
 	var rows = [1, 0, 0, 0, 0];
 	{
 		var b = $("#rows_to_show").serializeArray();
-		//console.log(b);
 		b.forEach(function(field, i) {
 			rows[field.name] = 1;
 		});
-		//console.log(rows);
 	}
 	
 	// Создаём HTML код таблицы и помещаем его на место div'а.
