2ecd5367cca7f884f157efe7a623a1d6bd27f283
Ivars Rozhleys <noname@noname.com>
2018-11-22 00:02:40 +0300

    Реализация треугольника Паскаля на PHP.

--- part-2.php
+++ part-2.php
@@ -1,3 +1,27 @@
 <?php
-	echo $_GET['N'];
+	$N = $_GET['N'];
+	$data = array($N);
+	echo "<table border=\"0\"><tbody>";
+	for($i = 0; $i < $N; ++$i) {
+		$data[$i] = array($i+1);
+		echo "<tr>";
+		for($j = 0; $j < $N; $j++) {
+			echo "<th width=\"40px\">";
+			if(($i + $j) % 2 == 0 && $j <= $i) { // <= условие активации клетки
+				if($i == $j) {
+					$data[$i][$j] = 1;
+				}
+				else if($i % 2 == 0 && $j == 0) {
+					$data[$i][$j] = 2 * $data[$i-1][$j+1];
+				}
+				else {
+					$data[$i][$j] = $data[$i-1][$j-1] + $data[$i-1][$j+1];
+				}
+				echo $data[$i][$j];
+			}
+			echo "</th>";
+		}
+		echo "</tr>";
+	}
+	echo "</tbody></table>";
 ?>
