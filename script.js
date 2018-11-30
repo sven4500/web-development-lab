15ae51be928e4296b3f2614485ef824978f5d4d1
Рожлейс Иварс <noname@noname.com>
2018-11-30 11:56:55 +0300

    Выводим квадратик на экран в точку куда скажет нам сервер.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-/*$(document).ready(function() {
-	console.log("Ready");
-});*/
+var ctx;
+var dim = {};
+
+$(document).ready(function() {
+	var cv = $("#example");
+	ctx = $(cv)[0].getContext("2d");
+	dim.width = ctx.canvas.clientWidth;
+	dim.height = ctx.canvas.clientHeight;
+});
 
 function onRedraw(data) {
-	console.log(data);
+	var pos = data.split(",");
+	ctx.clearRect(0, 0, dim.width, dim.height);
+	ctx.strokeRect(0, 0, dim.width, dim.height);
+	ctx.fillRect(pos[0], pos[1], 8, 8);
 }
 
 function onFrame() {
 	$.get("http://127.0.0.1:8000/data", {}, onRedraw);
 }
 
-setInterval(onFrame, 500);
+setInterval(onFrame, 100);
