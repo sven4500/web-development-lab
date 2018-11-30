5c5520610a678b715fb85ddc8bd24a4bf25558cd
Рожлейс Иварс <noname@noname.com>
2018-11-30 12:14:59 +0300

    Ограничиваю точку в пределах кадра.

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ $(document).ready(function() {
 	dim.height = ctx.canvas.clientHeight;
 });
 
-function onRedraw(data) {
+function onDataReady(data) {
 	var pos = data.split(",");
 	ctx.clearRect(0, 0, dim.width, dim.height);
 	ctx.strokeRect(0, 0, dim.width, dim.height);
-	ctx.fillRect(pos[0], pos[1], 8, 8);
+	ctx.fillRect(pos[0], pos[1], 10, 10);
 }
 
 function onFrame() {
-	$.get("http://127.0.0.1:8000/data", {}, onRedraw);
+	// Делаем пост на сервер на порт 8000. когда данные
+	// будут получены будет вызван метод onDataReady.
+	$.get("http://127.0.0.1:8000/data", {}, onDataReady);
 }
 
 setInterval(onFrame, 100);
--- server.js
+++ server.js
@@ -2,41 +2,42 @@
 var http = require("http");
 var url = require("url");
 
-// Устанавливаем обработчик HTTP запроса по 
+var dim = {"width": 640, "height": 480};
+var pos = {"x": dim.width / 2, "y": dim.height / 2};
+var nor = {"x": Math.random(), "y": Math.random()};
+var speed = 10;
+
+// Устанавливаем обработчик HTTP запроса на порт 8000.
 var server = http.createServer(onRequire);
 server.listen(8000);
-
-var dim = {"width": 640,
-	"height": 480};
-
-var pos = {"x": dim.width / 2,
-	"y": dim.height / 2};
-
-var nor = {"x": Math.random(),
-	"y": Math.random()};
-	
-var speed = 10;
 
 function onRequire(req, res) {
 	var path = url.parse(req.url).pathname;
 	if(path == "/data" && req.method == "GET") {
 		// Просто обязательно необходимо выставить заголовок Access-Control-Allow-Origin
-		// чтобы мы могли выполнить кросдоменный запрос.
+		// чтобы мы могли успешно выполнить кросдоменный запрос.
 		res.writeHead(200, {"Content-Type": "text/plain"});
 		res.writeHead(200, {"Access-Control-Allow-Origin": "*"});
 		res.write(String(pos.x) + "," + String(pos.y));
 		res.end();
-		console.log("Requested...");
 	}
 }
 
 function onFrame() {
+	// Каждый кадр добавляем к компоненте значения по нормали
+	// (строго говоря это конечно же не нормаль так как она не
+	// нормализована, однако для учебных целей думаю сойдёт).
+	// Если точка выбегает за кадр то оборачиваем компоненту.
 	pos.x += nor.x * speed;
 	pos.y += nor.y * speed;
-	if(pos.x < 0 || pos.x > dim.width)
+	if(pos.x < 0 || pos.x > dim.width) {
+		pos.x = Math.min(Math.max(pos.x, 0), dim.width);
 		nor.x *= -1;
-	if(pos.y < 0 || pos.y > dim.height)
+	}
+	if(pos.y < 0 || pos.y > dim.height) {
+		pos.y = Math.min(Math.max(pos.y, 0), dim.height);
 		nor.y *= -1;
+	}
 }
 
 setInterval(onFrame, 100);
