74709ad9eee9d6046301e9cc2b8ce6205a121867
Ivars Rozhleys <noname@noname.com>
2018-12-13 23:10:47 +0300

    Записи можно добавлять и удалять. Красота!

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 ﻿<html>
 	<head>
-		<b>Лабораторная работа №9.</b> phpMyAdmin.<br>
+		<b>Лабораторная работа №9.</b> Работа с СУБД.<br>
 		Рожлейс Иварс Андрисович.<br>
 		Группа 108.<hr>
+		Демо-пример на возможности phpMyAdmin.<hr>
 	</head>
 	<body>
 		<?php
@@ -15,12 +16,27 @@
 			}
 			$aut = $_GET["aut"];
 			$msg = $_GET["msg"];
+			$dat = $_GET["dat"];
+			// Если есть get запрос то обрабатываем его.
 			if($aut != NULL && $msg != NULL) {
+				// Добавляем запись в базу данных.
 				mysqli_query($conn, "INSERT INTO `messages` (`author`, `message`) VALUES ('$aut', '$msg');");
 			}
+			// Если в запросе пришло время значит нужно удалить запись.
+			if($dat != NULL) {
+				mysqli_query($conn, "DELETE FROM `messages` WHERE `date` = '$dat';");
+			}
+			// Получаем все данные из таблицы.
 			$res = mysqli_query($conn, "SELECT * FROM `messages`;");
 			while($r1 = mysqli_fetch_assoc($res)) {
-				echo $r1["author"], "<br><br>";
+				$aut = $r1["author"];
+				$msg = $r1["message"];
+				$dat = $r1["date"];
+				echo '<form method="GET" action="index.php" >';
+				echo '<button type="submit" name="dat" value="', $dat, '" >x</button>';
+				echo ' ';
+				echo $aut, "<br>", $dat, "<br>", $msg, "<br><br>";
+				echo '</form>';
 			}
 		?>
 		<hr>
